fa3d596a2210150200c4ccaa83f0c01f30960550
amr <amrfahmy57@gmail.com>
2020-10-23 00:40:01 +0200

    add triangle class

--- Geometry/main.cpp
+++ Geometry/main.cpp
@@ -8,6 +8,7 @@ class Line;
 class Polygon;
 class Rectangle;
 class LineSegment;
+class Triangle;
 
 class Point {
     public:
@@ -104,42 +105,75 @@ class Line{
 
 class Polygon{
     // Cases to handle: collinear points, polygons with n < 3
+    public:
+        static const double EPS = 1e-9;
+        
+        vector<Point> g; 			//first point = last point, counter-clockwise representation
+        
+        Polygon( vector<Point> o);
 
-	static const double EPS = 1e-9;
-	
-	vector<Point> g; 			//first point = last point, counter-clockwise representation
-	
-	Polygon( vector<Point> o);
-
-    double perimeter();
-    double area();
-    bool isConvex();
-    bool inside(Point p);
-    Polygon cutPolygon(Point a, Point b);
-    static Polygon convexHull(vector<Point> points);
-    Point centroid();
+        double perimeter();
+        double area();
+        bool isConvex();
+        bool inside(Point p);
+        Polygon cutPolygon(Point a, Point b);
+        static Polygon convexHull(vector<Point> points);
+        Point centroid();
 };
 
 class Rectangle {
+    public:
+        static const double EPS = 1e-9;
+        
+        Point ll, ur;
 
-	static const double EPS = 1e-9;
-	
-	Point ll, ur;
+        Rectangle(Point a, Point b);
 
-	Rectangle(Point a, Point b);
+        double area();
+        
+        bool contains(Point p);
 
-	double area();
-    
-	bool contains(Point p);
-
-	Rectangle intersect(Rectangle r);
+        Rectangle intersect(Rectangle r);
 
 };
 class LineSegment {
-	Point p, q;
-	LineSegment(Point a, Point b);
-	bool intersect(LineSegment ls);
+    public:
+        Point p, q;
+        LineSegment(Point a, Point b);
+        bool intersect(LineSegment ls);
 };
+
+
+class Triangle {
+
+	static const double EPS = 1e-9;
+	
+	Point a, b, c;
+	double ab, bc, ca;
+
+	Triangle(Point p, Point q, Point r);
+
+	double perm();
+
+	double area();
+
+	double area2() ;
+
+	double area3();
+    double rInCircle();
+
+	Point inCenter();
+
+	double rCircumCircle();
+
+	Point circumCircle() ;
+
+	static double areaMedians(double ma, double mb, double mc);
+
+	static double areaHeights(double ha, double hb, double hc);
+};
+
+
 
 Point::Point(double a ,double b){
     x = a; 
@@ -460,6 +494,85 @@ bool LineSegment::intersect(LineSegment ls){
     return c.between(p, q) && c.between(ls.p, ls.q);
 }
 
+Triangle::Triangle(Point p, Point q, Point r)	//counter clockwise
+{
+    a = p;
+    if(Point::ccw(p, q, r)) { b = q; c = r; }
+    else { b = r; c = q; }
+
+    ab = a.dist(b); bc = b.dist(c); ca = c.dist(a);
+}
+
+double Triangle::perm()
+{
+    return ab + bc + ca;
+}
+
+double Triangle::area()
+{
+    double s = (ab + bc + ca) / 2.0;
+    return sqrt(s * (s - ab) * (s - bc) * (s - ca));		//take care of overflow
+}
+
+double Triangle::area2() 
+{  return abs(a.x * b.y - a.y * b.x + b.x * c.y - b.y * c.x + c.x * a.y - c.y * a.x) / 2.0; }
+	/*
+	 * 1. We take the absolute value because the result could be negative which means that point q is on
+	 *    the left of the line segment pr
+	 * 
+	 * 2. If the area is zero, then the three points are collinear
+	 */
+
+double Triangle::area3()
+{
+    return 0.5 * ((c.y - a.y) * (b.x - a.x) - (b.y - a.y) * (c.x - a.x));
+}
+double Triangle::rInCircle()
+{
+    return area() / (perm() * 0.5);
+}
+
+Point Triangle::inCenter()				//intersection of the angle bisectors, center of inscribed circle
+{
+    double p = perm();
+    double x = (a.x * bc + b.x * ca + c.x * ab) / p;
+    double y = (a.y * bc + b.y * ca + c.y * ab) / p;
+    return Point(x, y);
+}
+
+double Triangle::rCircumCircle()
+{
+    return ab * bc * ca / (4.0 * area());
+}
+
+Point Triangle::circumCircle() 
+{
+    double bax = b.x - a.x, bay = b.y - a.y;
+    double cax = c.x - a.x, cay = c.y - a.y;
+
+    double e = bax * (a.x + b.x) + bay * (a.y + b.y);
+    double f = cax * (a.x + c.x) + cay * (a.y + c.y);
+    double g = 2.0 * (bax * (c.y - b.y) - bay * (c.x - b.x));
+
+    // THIS SHOULD BE IN YOUR MAIN
+    // if (abs(g) < EPS) return NULL;
+
+    return Point((cay*e - bay*f) / g, (bax*f - cax*e) / g);
+
+}
+
+double Triangle::areaMedians(double ma, double mb, double mc)		//medians of the triangle
+{
+    double s = (ma + mb + mc) / 2.0;							//max(ma, mb, mc) < s
+    return sqrt(s * (s - ma) * (s - mb) * (s - mc)) * 4.0 / 3.0;
+}
+
+double Triangle::areaHeights(double ha, double hb, double hc)		//heights of the triangle
+{
+    double ha_1 = 1.0 / ha, hb_1 = 1.0 / hb, hc_1 = 1.0 / hc;
+    double s = (ha_1 + hb_1 + hc_1) / 2.0;
+    return 1.0 / (sqrt(s * (s - ha_1) * (s - hb_1) * (s - hc_1)) * 4.0);
+}
 class Geometry {
 
 	static const double INF = 1e9, EPS = 1e-9;		// better use 1e-11 for large coordinates and 1e-15 if infinite precision is required
